18910c13dae811286ee57557be2c6958a2e14e2a
albert234567 <albert.roig1@insbaixcamp.cat>
2024-06-04 20:15:27 +0200

    es para el audio al tocar el mar

--- js.js
+++ js.js
@@ -3,6 +3,12 @@ function reproduirAudio() {
     player.play();
 }
 
+function pararAudio() {
+    let player = document.getElementById('player');
+    player.pause();
+    player.currentTime = 0; // Reiniciar l'àudio
+}
+
 function iniciarAnimacio() {
     let nau = document.querySelector('.nauTallada');
     nau.style.animationPlayState = 'running';
@@ -10,10 +16,16 @@ function iniciarAnimacio() {
 
 window.onload = function() {
     let playButton = document.getElementById('playButton');
-    
+    let nau = document.querySelector('.nauTallada');
+
     // Afegir l'event listener al botó de play
     playButton.addEventListener('click', function() {
         reproduirAudio();
         iniciarAnimacio();
+    });
+
+    // Afegir l'event listener per parar l'àudio quan l'animació acabi
+    nau.addEventListener('animationend', function() {
+        pararAudio();
     });
 };
